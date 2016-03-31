39d8d3699bc70a07c52c587877fe6c564e08dd7f
andreabello <andrea91bis@hotmail.it>
2016-03-31 18:37:40 +0200

    chessboard

--- app/src/main/java/bello/andrea/canvaschessboard/MainActivity.java
+++ app/src/main/java/bello/andrea/canvaschessboard/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends Activity {
 
         final RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.container);
 
-
         relativeLayout.post(new Runnable() {
             @Override
             public void run() {
@@ -28,12 +27,61 @@ public class MainActivity extends Activity {
                 paint.setColor(ContextCompat.getColor(MainActivity.this, android.R.color.black));
 
                 Canvas canvas = new Canvas(background);
-                canvas.drawRect(50, 50, 200, 200, paint);
-                canvas.drawRect(350, 50, 500, 200, paint);
-                canvas.drawRect(200, 200, 350, 350, paint);
 
-                relativeLayout.setBackground(new BitmapDrawable(getResources(), background));
+                int chessBoardSide = 8;
+                int padding = 100;
+                int borderWidth = 100;
+                int minDimension;
+                if(relativeLayout.getWidth() < relativeLayout.getHeight())
+                    minDimension = relativeLayout.getWidth();
+                else
+                    minDimension = relativeLayout.getHeight();
+
+                int squareSide = (minDimension - 2 * padding - 2*borderWidth)/chessBoardSide;
+
+                for(int rawIndex=0; rawIndex<chessBoardSide; rawIndex++){
+                    drawRow(canvas, padding, rawIndex, chessBoardSide, squareSide, borderWidth);
+                }
+
+                drawBorder(canvas, padding, squareSide, chessBoardSide, borderWidth);
+
+                relativeLayout.setBackgroundDrawable(new BitmapDrawable(getResources(), background));
             }
         });
+
+    }
+
+    private void drawRow(Canvas canvas, int padding, int rowIndex, int columnCount, int sideDimension, int borderWidth){
+        Paint lightPaint = new Paint();
+        lightPaint.setColor(ContextCompat.getColor(this, android.R.color.white));
+
+        Paint darkPaint = new Paint();
+        darkPaint.setColor(ContextCompat.getColor(this, android.R.color.black));
+
+        for(int columnIndex=0; columnIndex<columnCount; columnIndex++){
+            int left = padding + borderWidth + columnIndex*sideDimension;
+            int top = padding + borderWidth + rowIndex*sideDimension;
+            int right = padding + borderWidth + columnIndex*sideDimension + sideDimension;
+            int bottom = padding + borderWidth + rowIndex*sideDimension + sideDimension;
+
+            if(columnIndex%2 == rowIndex%2)
+                canvas.drawRect(left, top, right, bottom, darkPaint);
+            else
+                canvas.drawRect(left, top, right, bottom, lightPaint);
+        }
+    }
+
+    private void drawBorder(Canvas canvas, int padding, int cellDimension, int chessBoardSide, int borderWidth){
+        Paint borderPaint = new Paint();
+        borderPaint.setColor(ContextCompat.getColor(this, android.R.color.holo_purple));
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(borderWidth);
+
+        int left = padding + borderWidth/2;
+        int top = padding + borderWidth/2;
+        int right = padding + 3*borderWidth/2 + chessBoardSide*cellDimension;
+        int bottom = padding + 3*borderWidth/2 + chessBoardSide*cellDimension;
+
+        canvas.drawRect(left, top, right, bottom, borderPaint);
     }
 }
